986a67dfef66febde0e453208c3e9213fb6a1c26
NelGabAr <59204203+NelGabAr@users.noreply.github.com>
2025-02-26 23:47:33 +0100

    first commit
    starting the project without classes

---
+++ src/main/java/com/example/footballmadrid/FootballMadridApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballmadrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballMadridApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballMadridApplication.class, args);
+    }
+
+}
