c477b8e371d8511c6887849a5db74552d093464c
nelso <nelsongabrielarze@hotmail.com>
2025-04-04 18:28:22 +0200

    changed RestController anotation to Controller

--- src/main/java/com/example/footballmadrid/controllers/UserController.java
+++ src/main/java/com/example/footballmadrid/controllers/UserController.java
@@ -9,13 +9,14 @@ import com.example.footballmadrid.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class UserController {
     @Autowired
